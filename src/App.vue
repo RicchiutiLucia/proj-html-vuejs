949d123100f6e19898f00a25281e43b6ec139c57
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-04-11 14:59:26 +0200

    header completato rendendo dinamica la navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,65 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+
 
 </script>
 
 <template>
+    <body>
+        <div class="background container height728">
+            <AppHeader/>
+        <div class="container-small heightBanner flex-center">
+        <div class="container-Title">
+          <h1 class="title">NO PAIN NO GAIN</h1>
+          <p>Motivation is what gets you started. Habit is what keeps you going </p>
+          <button>JOIN US TODAY</button>
+        </div>
+      </div>
+    </div>
+        
+    </body>
+    
   
 </template>
 
-<style>
+<style lang="scss">
+@import './styles/generals.scss';
+@import './styles/_variable.scss';
+.background {
+  background-image: url('assets/images/home-banner.jpg');
+  background-size: cover;
+}
+.container {
+  width: 100%;
+}
+.title {
+  font-size: $fontSize65;
+  color: $colorWhite;
+  font-weight: bold;
+}
+.container-Title {
+  margin-top: 75px;
+  color: $colorWhite;
+  text-align: center;
+    p {
+      font-size: 16px;
+      color: lightgray;
+      padding: 15px 15px
+    }
+    button {
+      border: 1px solid gray; 
+      padding: 10px 30px;
+      background-color: inherit;
+      color: gray;
+      cursor: pointer;
+    }
+}
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <header class="containerHeader">
+      <div class="logo">
+        <div class="containerImg">
+         <a href="#"><img src="../assets/images/gym_logo_2x.png" alt=""> </a>
+        </div>
+      </div>
+      <div class="link flex">
+        <ul class="ul" v-for="(link,index) in links" :key="index">
+          <li >
+            <a :class="(link.active == true) ? 'active' : null" href="#">{{link.text}}</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  </div>
+    
+</template>
+  
+<script>
+export default {
+  name: 'AppHeader',
+  data(){
+    return{
+        links:[
+        {
+          text: 'HOME',
+          url: '#',
+          active: true
+        },
+        {
+          text: 'ABOUT US',
+          url: '#',
+          active: false
+        },
+        {
+          text: 'FACILITIES',
+          url: '#',
+          active: false
+        },
+        {
+          text: 'MEMBERSHIP',
+          url: '#',
+          active: false
+        },
+        {
+          text: 'TESTIMONIALS',
+          url: '#',
+          active: false
+        },
+        {
+          text: 'BLOG',
+          url: '#',
+          active: false
+        },
+
+        ]
+    }
+  }
+}
+  </script>
+  
+<style lang="scss" scoped>
+  @import '../styles/generals.scss';
+  @import '../styles/variable.scss';
+.containerHeader {
+  width: 100%;
+  height: 240px;
+  display: flex;
+    .logo {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      padding: 100px;
+        .containerImg {
+          width: 170px;
+          height: 100px;
+            img {
+              width: 100%;
+              height: 100%;
+            }
+        }
+    }
+    .link {
+      width: 50%;
+      height: 100%;
+        .ul {
+          padding-right: 20px;
+            li {
+              list-style-type: none;
+                a {
+                  text-decoration: none;
+                  color: $colorWhite;
+                  font-size: $fontSize13;
+                }
+                a.active {
+                  color:$colorRed;
+                }
+            }
+        }
+    }
+}
+  
+  </style>
